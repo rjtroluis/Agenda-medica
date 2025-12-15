3d04b0c9a29cbe8fc8e43965af11cd4cd5aec7a2
Correa Dominguez Luis Angel <148934422+rjtroluis@users.noreply.github.com>
2025-12-15 09:38:51 -0600

    Add files via upload

---
+++ includes/db_config.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('America/Mexico_City');
+const DB_HOST = "localhost";
+const DB_NAME = "directorio_medico";
+const DB_USER = "root";
+const DB_PASS = "Rjtroller1"; 
+?>
